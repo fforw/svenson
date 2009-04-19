512791eedc1472c79396161da2d1ac96b422b460
fforw@gmx.de <fforw@gmx.de@77c52dbe-9927-11dd-bd51-a78b85c98fae>
2009-04-19 15:19:25 +0000

    git-svn-id: http://svenson.googlecode.com/svn/trunk@67 77c52dbe-9927-11dd-bd51-a78b85c98fae

--- src/org/svenson/ObjectFactory.java
+++ src/org/svenson/ObjectFactory.java
@@ -2,6 +2,18 @@ package org.svenson;
 
 public interface ObjectFactory<T>
 {
+    /**
+     * Returns <code>true</code> if the factory can create objects of the given class
+     * 
+     * @param cls
+     * @return
+     */
     boolean supports(Class<T> cls);
+    
+    /**
+     * Creates an instance of the given type.
+     * @param typeHint
+     * @return
+     */
     T create(Class<T> typeHint);
 }
