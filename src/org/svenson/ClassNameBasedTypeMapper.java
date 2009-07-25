54e465842d7ac1e7fa9c315a46bed237fdcc47d6
fforw@gmx.de <fforw@gmx.de@77c52dbe-9927-11dd-bd51-a78b85c98fae>
2009-07-25 11:25:50 +0000

    optionally enforce base type in ClassNameBasedTypeMapper
    git-svn-id: http://svenson.googlecode.com/svn/trunk@80 77c52dbe-9927-11dd-bd51-a78b85c98fae

--- src/org/svenson/ClassNameBasedTypeMapper.java
+++ src/org/svenson/ClassNameBasedTypeMapper.java
@@ -4,6 +4,24 @@ public class ClassNameBasedTypeMapper extends AbstractPropertyValueBasedTypeMapp
 {
     private String basePackage;
     
+    private Class enforcedBaseType;
+    
+    /**
+     * Sets the base type to which all created instances must be assignable to.
+     * 
+     * @param enforcedBaseType
+     */
+    public void setEnforcedBaseType(Class enforcedBaseType)
+    {
+        this.enforcedBaseType = enforcedBaseType;
+    }
+    
+    /**
+     * Sets the base package that is put before the given type to form the fully qualified
+     * class names.
+     * 
+     * @param basePackage
+     */
     public void setBasePackage(String basePackage)
     {
         this.basePackage = basePackage;
@@ -26,7 +44,12 @@ public class ClassNameBasedTypeMapper extends AbstractPropertyValueBasedTypeMapp
         
         try
         {
-            return Class.forName(value);
+            Class cls = Class.forName(value);
+            if (enforcedBaseType != null && !enforcedBaseType.isAssignableFrom(cls))
+            {
+                throw new IllegalStateException(cls + " is not assignable to " + enforcedBaseType);
+            }
+            return cls;
         }
         catch (ClassNotFoundException e)
         {
