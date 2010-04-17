4518217ec169c818dd58aeb87d51d3377dcf67af
fforw@gmx.de <fforw@gmx.de@77c52dbe-9927-11dd-bd51-a78b85c98fae>
2010-04-17 11:17:55 +0000

    dump/parse cycle for min and max long values
    git-svn-id: http://svenson.googlecode.com/svn/trunk@137 77c52dbe-9927-11dd-bd51-a78b85c98fae

---
+++ test/org/svenson/NumbersTestCase.java
@@ -0,0 +1,52 @@
+package org.svenson;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class NumbersTestCase
+{
+    @Test
+    public void thatMinAndMaxWork()
+    {
+        String minJSON = JSON.defaultJSON().forValue(new Bean(Long.MIN_VALUE));
+        String maxJSON = JSON.defaultJSON().forValue(new Bean(Long.MAX_VALUE));
+        assertThat(minJSON, is("{\"value\":" + Long.MIN_VALUE + "}"));
+        assertThat(maxJSON, is("{\"value\":" + Long.MAX_VALUE + "}"));
+
+        Bean minBean = JSONParser.defaultJSONParser().parse(Bean.class, minJSON);
+        Bean maxBean = JSONParser.defaultJSONParser().parse(Bean.class, maxJSON);
+        
+        assertThat(minBean.getValue(), is(Long.MIN_VALUE));
+        assertThat(maxBean.getValue(), is(Long.MAX_VALUE));
+        
+    }
+    
+    public static class Bean
+    {
+        private long value;
+
+        public Bean()
+        {
+            
+        }
+        
+        public Bean(long value)
+        {
+            this.value = value;
+        }
+
+        public long getValue()
+        {
+            return value;
+        }
+
+        public void setValue(long value)
+        {
+            this.value = value;
+        }
+        
+        
+    }
+}
