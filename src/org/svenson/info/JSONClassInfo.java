1f38d7e5779128a90e641f130515d1707bd5091f
Sven Helmberger <fforw@gmx.de>
2011-03-24 22:12:05 +0100

    move addMethod and TypeHint handling to new JSONClassInfo infrastructure. Convert JSON to JSONClassInfo

---
+++ src/org/svenson/info/JSONClassInfo.java
@@ -0,0 +1,239 @@
+package org.svenson.info;
+
+import java.beans.Introspector;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.svenson.JSONParseException;
+import org.svenson.JSONProperty;
+import org.svenson.JSONReference;
+import org.svenson.JSONTypeHint;
+
+public class JSONClassInfo
+{
+
+    private static final String ADDER_PREFIX = "add";
+
+    private static final String SETTER_PREFIX = "set";
+
+    private static final String GETTER_PREFIX = "get";
+
+    private static final String ISSER_PREFIX = "is";
+
+    private static ConcurrentMap<Class<?>, JSONClassInfo> holders;
+    static
+    {
+        clear();
+    }
+    
+    private Class cls;
+
+    private Map<String, JSONPropertyInfo> propertyInfos;
+
+    public JSONClassInfo(Class cls)
+    {
+        this.cls = cls;
+    }
+
+    public synchronized void init()
+    {
+        if (this.propertyInfos != null)
+        {
+            return;
+        }
+        
+        Map<String, JSONPropertyInfo> propertyInfos = new HashMap<String, JSONPropertyInfo>();
+
+        for (Method m : cls.getMethods())
+        {
+            String name = m.getName();
+
+            if ((m.getModifiers() & Modifier.PUBLIC) == 0 || name.equals("getClass"))
+            {
+                continue;
+            }
+
+            if (name.startsWith(SETTER_PREFIX) && m.getParameterTypes().length == 1)
+            {
+                String javaPropertyName = propertyName(name, SETTER_PREFIX.length());
+                JSONPropertyInfo pair = propertyInfos.get(javaPropertyName);
+                if (pair != null)
+                {
+                    pair.setSetterMethod(m);
+                }
+                else
+                {
+                    pair = new JSONPropertyInfo(javaPropertyName, null, m);
+                    propertyInfos.put(javaPropertyName, pair);
+                }
+                
+                Class<?>[] parameterTypes;
+                Class<?> paramType;
+                if ((parameterTypes = m.getParameterTypes()).length == 1 && ( paramType = parameterTypes[0]).isArray())
+                {
+                    pair.setTypeHint(paramType.getComponentType());
+                }
+                
+            }
+            else if (m.getParameterTypes().length == 0 && !m.getReturnType().equals(void.class))
+            {
+                if (name.startsWith(GETTER_PREFIX))
+                {
+                    String javaPropertyName = propertyName(name, GETTER_PREFIX.length());
+                    JSONPropertyInfo pair = propertyInfos.get(javaPropertyName);
+                    if (pair != null)
+                    {
+                        pair.setGetterMethod(m);
+                    }
+                    else
+                    {
+                        propertyInfos.put(javaPropertyName, new JSONPropertyInfo(javaPropertyName, m, null));
+                    }
+                }
+                else if (name.startsWith(ISSER_PREFIX))
+                {
+                    String javaPropertyName = propertyName(name, ISSER_PREFIX.length());
+                    JSONPropertyInfo pair = propertyInfos.get(javaPropertyName);
+                    if (pair != null)
+                    {
+                        pair.setGetterMethod(m);
+                    }
+                    else
+                    {
+                        propertyInfos.put(javaPropertyName, new JSONPropertyInfo(javaPropertyName, m, null));
+                    }
+                }
+            }
+            else if (name.startsWith(ADDER_PREFIX) && m.getParameterTypes().length == 1 )
+            {
+                String javaPropertyName = propertyName(name, ADDER_PREFIX.length());
+                JSONPropertyInfo pair = propertyInfos.get(javaPropertyName);
+                if (pair != null)
+                {
+                    pair.setAdderMethod(m);
+                }
+                else
+                {
+                    JSONPropertyInfo newInfo = new JSONPropertyInfo(javaPropertyName, null, null);
+                    newInfo.setAdderMethod(m);
+                    propertyInfos.put(javaPropertyName, newInfo);
+                }
+            }
+        }
+
+        this.propertyInfos = new HashMap<String, JSONPropertyInfo>();
+
+        for (Map.Entry<String, JSONPropertyInfo> e : propertyInfos.entrySet())
+        {
+            String jsonPropertyName = e.getKey();
+            JSONPropertyInfo propertyInfo = e.getValue();
+
+            Method getterMethod = propertyInfo.getGetterMethod();
+            Method setterMethod = propertyInfo.getSetterMethod();
+            JSONProperty jsonProperty = getAnnotation(JSONProperty.class, getterMethod,
+                setterMethod);
+
+            if (jsonProperty != null)
+            {
+                if (jsonProperty.value().length() > 0)
+                {
+                    jsonPropertyName = jsonProperty.value();
+                }
+
+                propertyInfo.setIgnore(jsonProperty.ignore());
+                propertyInfo.setIgnoreIfNull(jsonProperty.ignoreIfNull());
+                propertyInfo.setReadOnly(jsonProperty.readOnly());
+            }
+            propertyInfo.setJsonName(jsonPropertyName);
+
+            JSONReference refAnno = getAnnotation(JSONReference.class, getterMethod,
+                setterMethod);
+            
+            if (refAnno != null)
+            {
+                propertyInfo.setLinkIdProperty(refAnno.idProperty());
+            }
+            
+            JSONTypeHint typeHintAnno = getAnnotation(JSONTypeHint.class, getterMethod, setterMethod);
+            Class<?>[] parameterTypes;
+            Class paramType;
+            if (typeHintAnno != null)
+            {
+                propertyInfo.setTypeHint(typeHintAnno.value());
+            }
+            
+            this.propertyInfos.put(jsonPropertyName, propertyInfo);
+        }
+    }
+
+
+    private static <T extends Annotation> T getAnnotation(Class<T> cls, Method readMethod,
+        Method writeMethod)
+    {
+        T anno = null;
+        if (readMethod != null)
+        {
+            anno = readMethod.getAnnotation(cls);
+        }
+        if (anno == null && writeMethod != null)
+        {
+            anno = writeMethod.getAnnotation(cls);
+        }
+        return anno;
+    }
+
+
+    private static String propertyName(String name, int prefixLen)
+    {
+        return Introspector.decapitalize(name.substring(prefixLen));
+    }
+
+
+        
+    public JSONPropertyInfo getPropertyInfo(String jsonPropertyName)
+    {
+        return propertyInfos.get(jsonPropertyName);
+    }
+
+
+    public Set<String> getPropertyNames()
+    {
+        return propertyInfos.keySet();
+    }
+    
+    public Collection<JSONPropertyInfo> getPropertyInfos()
+    {
+        return propertyInfos.values();
+    }
+
+    public static JSONClassInfo forClass(Class cls)
+    {
+        JSONClassInfo holder = new JSONClassInfo(cls);
+        JSONClassInfo existing = holders.putIfAbsent(cls, holder);
+        if (existing != null)
+        {
+            holder = existing;
+        }
+        
+        holder.init();
+        return holder;
+    }
+    
+    /**
+     * Release all class infos.
+     * 
+     */
+    public static void clear()
+    {
+        holders = new ConcurrentHashMap<Class<?>, JSONClassInfo>();
+    }
+    
+}
---
+++ src/org/svenson/info/JSONPropertyInfo.java
@@ -0,0 +1,231 @@
+package org.svenson.info;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.svenson.JSONParseException;
+import org.svenson.util.ExceptionWrapper;
+
+public class JSONPropertyInfo
+{
+    private Method getterMethod;
+
+    private Method setterMethod;
+
+    private Method adderMethod;
+
+    private boolean ignore, ignoreIfNull, readOnly;
+    
+    private String javaPropertyName;
+
+    private Class typeHint;
+
+    private String jsonName;
+    
+    private String linkIdProperty;
+    
+    public JSONPropertyInfo(String javaPropertyName, Method getterMethod, Method setterMethod)
+    {
+        this.javaPropertyName = javaPropertyName;
+        this.getterMethod = getterMethod;
+        this.setterMethod = setterMethod;
+    }
+
+
+    public Method getSetterMethod()
+    {
+        return setterMethod;
+    }
+
+
+    public void setSetterMethod(Method setterMethod)
+    {
+        this.setterMethod = setterMethod;
+    }
+
+
+    public Method getGetterMethod()
+    {
+        return getterMethod;
+    }
+
+
+    public void setGetterMethod(Method getterMethod)
+    {
+        this.getterMethod = getterMethod;
+    }
+
+
+    public boolean isIgnore()
+    {
+        return ignore;
+    }
+
+
+    public void setIgnore(boolean ignore)
+    {
+        this.ignore = ignore;
+    }
+
+
+    public boolean isIgnoreIfNull()
+    {
+        return ignoreIfNull;
+    }
+
+
+    public void setIgnoreIfNull(boolean ignoreIfNull)
+    {
+        this.ignoreIfNull = ignoreIfNull;
+    }
+
+
+    public boolean isReadOnly()
+    {
+        return readOnly;
+    }
+
+
+    public void setReadOnly(boolean readOnly)
+    {
+        this.readOnly = readOnly;
+    }
+
+
+    public String getJavaPropertyName()
+    {
+        return javaPropertyName;
+    }
+
+
+    public void setJavaPropertyName(String javaPropertyName)
+    {
+        this.javaPropertyName = javaPropertyName;
+    }
+
+
+    public boolean isLinkedProperty()
+    {
+        return linkIdProperty != null;
+    }
+
+    public String getLinkIdProperty()
+    {
+        return linkIdProperty;
+    }
+
+
+    public void setLinkIdProperty(String linkIdProperty)
+    {
+        this.linkIdProperty = linkIdProperty;
+    }
+
+
+    public Class getTypeOfProperty()
+    {
+        if (setterMethod != null)
+        {
+            return setterMethod.getParameterTypes()[0];
+        }
+        return null;
+    }
+
+
+    public boolean isWriteable()
+    {
+        return setterMethod != null;
+    }
+
+
+    public boolean isReadable()
+    {
+        return getterMethod != null;
+    }
+    
+    
+    public Class getTypeHint()
+    {
+        return typeHint;
+    }
+
+
+    public void setTypeHint(Class typeHint)
+    {
+        this.typeHint = typeHint;
+    }
+    
+    public Method getAdderMethod()
+    {
+        return adderMethod;
+    }
+
+
+    public void setAdderMethod(Method adderMethod)
+    {
+        this.adderMethod = adderMethod;
+    }
+
+    public String getJsonName()
+    {
+        return jsonName;
+    }
+
+
+    public void setJsonName(String jsonName)
+    {
+        this.jsonName = jsonName;
+    }
+
+
+    public Object getProperty(Object target)
+    {
+        Method getterMethod = getGetterMethod();
+        if (getterMethod == null)
+        {
+            throw new JSONParseException("Property '" + getJavaPropertyName() + "' in " + target.getClass() + " is not readable.");
+        }
+        
+        try
+        {
+            return getterMethod.invoke(target);
+        }
+        catch (IllegalAccessException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (InvocationTargetException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+    }
+    
+    public void setProperty(Object target, Object value)
+    {
+        Method setterMethod = getSetterMethod();
+        if (setterMethod == null)
+        {
+            throw new JSONParseException("Property '" + getJavaPropertyName() + "' in " + target.getClass() + " is not writable.");
+        }
+
+        try
+        {
+            setterMethod.invoke(target, value);
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (IllegalAccessException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+        catch (InvocationTargetException e)
+        {
+            throw ExceptionWrapper.wrap(e);
+        }
+    }
+
+    
+    
+    
+}
